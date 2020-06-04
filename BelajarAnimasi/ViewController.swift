d07272a4c2fc8c0f58414ab12b461ad9d28b3915
Haryanto Quang <quang.haryanto@gmail.com>
2020-06-04 19:49:45 +0700

    initial project

---
+++ BelajarAnimasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelajarAnimasi
+//
+//  Created by Haryanto Salim on 04/06/20.
+//  Copyright © 2020 Haryanto Salim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
